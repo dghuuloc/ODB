01961a6d47846a70da883af1046223f4a5e4c51e
dghuuloc <87122072+dghuuloc@users.noreply.github.com>
2022-07-04 11:38:09 +0700

    creating a customers table

--- sampledb.sql
+++ sampledb.sql
@@ -43,3 +43,41 @@ BEGIN
       dbms_output.put_line('Inner Variable num2: ' || num2); 
    END;  
 END; 
+
+
+--------------------------------------------------------------------
+-- Create a customer table
+--------------------------------------------------------------------
+
+CREATE TABLE customers (
+	id INT NOT NULL,
+	name VARCHAR2 (20) NOT NULL,
+	age INT NOT NULL,
+	address CHAR (25),
+	salary DECIMAL (18,2),
+	PRIMARY KEY (id)
+);
+
+-- Insert some values into customer table
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) 
+VALUES (1, 'Ramesh', 32, 'Ahmedabad', 2000.00 );  
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) 
+VALUES (2, 'Khilan', 25, 'Delhi', 1500.00 );  
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) 
+VALUES (3, 'kaushik', 23, 'Kota', 2000.00 );
+  
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) 
+VALUES (4, 'Chaitali', 25, 'Mumbai', 6500.00 ); 
+ 
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) 
+VALUES (5, 'Hardik', 27, 'Bhopal', 8500.00 );  
+
+INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) 
+VALUES (6, 'Komal', 22, 'MP', 4500.00 );
+
+
+
+
