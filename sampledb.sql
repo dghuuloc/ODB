78f9b56552dcfc6e3b0d456f2b05a809c8a65b1d
dghuuloc <87122072+dghuuloc@users.noreply.github.com>
2022-07-04 11:28:31 +0700

    adding scope variable in PL/SQL

--- sampledb.sql
+++ sampledb.sql
@@ -12,7 +12,6 @@
 
 -- create anonymous block 
 
-
 DECALRE
 	-- Variable Declaration
 	message VARCHAR2(50) := 'Hello, World!';
@@ -28,3 +27,19 @@ END;
 -- Variable Scope in PL/SQL
 --------------------------------------------------------------------
 
+DECLARE 
+   -- Global variables  
+   num1 number := 95;  
+   num2 number := 85;  
+BEGIN  
+   dbms_output.put_line('Outer Variable num1: ' || num1); 
+   dbms_output.put_line('Outer Variable num2: ' || num2); 
+   DECLARE  
+      -- Local variables 
+      num1 number := 195;  
+      num2 number := 185;  
+   BEGIN  
+      dbms_output.put_line('Inner Variable num1: ' || num1); 
+      dbms_output.put_line('Inner Variable num2: ' || num2); 
+   END;  
+END; 
