aef04a5e5df7e2255f5a6b10ec52b49cb5f90d0a
dghuuloc <87122072+dghuuloc@users.noreply.github.com>
2022-07-04 13:31:05 +0700

    Update sampledb.sql

--- sampledb.sql
+++ sampledb.sql
@@ -79,6 +79,20 @@ VALUES (5, 'Hardik', 27, 'Bhopal', 8500.00 );
 INSERT INTO CUSTOMERS (ID,NAME,AGE,ADDRESS,SALARY) 
 VALUES (6, 'Komal', 22, 'MP', 4500.00 );
 
+--------------------------------------------------------------------
+-- Using SELECT INTO clause of SQL
+--------------------------------------------------------------------
 
-
+DECLARE 
+   c_id customers.id%TYPE := 1; 
+   c_name  customers.name%TYPE; 
+   c_addr customers.address%TYPE; 
+   c_sal  customers.salary%TYPE; 
+BEGIN 
+   SELECT name, address, salary INTO c_name, c_addr, c_sal 
+   FROM customers 
+   WHERE id = c_id;  
+   dbms_output.put_line 
+   ('Customer ' ||c_name || ' from ' || c_addr || ' earns ' || c_sal); 
+END;
 
