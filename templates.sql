a2ec8fb013b086d1d33b2ac3126a0e18126321a4
dghuuloc <87122072+dghuuloc@users.noreply.github.com>
2022-07-06 08:58:25 +0700

    Create templates.sql

---
+++ templates.sql
@@ -0,0 +1,21 @@
+--------------------------------------------------------------------------------------
+-- Name	       : (Oracle Tutorial) Sample Database
+-- Version     : 1.0
+-- Last Updated: July-04-2022
+-- Copyright   : Copyright © 2022 by dghuuloc. All Rights Reserved.
+-- Notice      : Use this sample database for the educational purpose only.
+
+--------------------------------------------------------------------------------------
+--------------------------------------------------------------------
+-- PACKAGES --
+--------------------------------------------------------------------
+
+-- create package specification
+CREATE OR REPLACE PACKAGE package_name AS
+    FUNCTION function_name (list_variable) RETURN data_type;
+    
+    PROCEDURE procedure_name (list_variable);
+    
+END package_name;
+
+
